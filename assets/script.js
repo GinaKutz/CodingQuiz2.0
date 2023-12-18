8058b2eb54f3eeb7c2c4f7a031e93de81d84b29c
GinaKutz <gjkutz@gmail.com>
2023-12-18 10:51:22 -0600

    adding in questions

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,96 @@
 document.addEventListener('DOMContentLoaded', () => {
     const quizData = [
-      {
-        question: "What is the capital of France?",
-        options: ["Paris", "Berlin", "Madrid", "Rome"],
-        answer: "Paris"
-      },
-      {
-        question: "Which planet is known as the Red Planet?",
-        options: ["Jupiter", "Mars", "Venus", "Mercury"],
-        answer: "Mars"
-      },
+        {
+            numb: 1,
+            question: "What does HTML stand for?",
+            answer: "Hyper Text Markup Language",
+            options: [
+              "Hyper Text Preprocessor",
+              "Hyper Text Markup Language",
+              "Hyper Text Multiple Language",
+              "Hyper Tool Multi Language"
+            ]
+          },
+            {
+            numb: 2,
+            question: "What does CSS stand for?",
+            answer: "Cascading Style Sheet",
+            options: [
+              "Common Style Sheet",
+              "Colorful Style Sheet",
+              "Computer Style Sheet",
+              "Cascading Style Sheet"
+            ]
+          },
+            {
+            numb: 3,
+            question: "Which of the following is not a programming language?",
+            answer: "CSS",
+            options: [
+              "Python",
+              "CSS",
+              "Java",
+              "Ruby"
+            ]
+          },
+            {
+            numb: 4,
+            question: "In Python, what is used to inticate a block of code following a conditional statement or loop",
+            answer: "Indentation",
+            options: [
+              "Parenthesis",
+              "Curly Brackets",
+              "Indentation",
+              "Quotation marks"
+            ]
+          },
+            {
+            numb: 5,
+            question: "What does XML stand for?",
+            answer: "eXtensible Markup Language",
+            options: [
+              "eXtensible Markup Language",
+              "eXecutable Multiple Language",
+              "eXTra Multi-Program Language",
+              "eXamine Multiple Language"
+            ]
+          },
+        
+          {
+            numb: 6,
+            question: "Which symbol is used for a single-line comment in most programming language?",
+            answer: "//",
+            options: [
+              "//",
+              "#",
+              "--",
+              "/**/"
+            ]
+          },
+        
+          {
+            numb: 7,
+            question: 'What is the purpose of the "git" version control system?',
+            answer: "To manage and track changes in files",
+            options: [
+              "To write and execute code",
+              "To manage and track changes in files",
+              "To design user interface",
+              "To optimize website loading speed"
+            ]
+          },
+        
+          {
+            numb: 8,
+            question: "What does the acronym API stand for in programming?",
+            answer: "Application Programming Interface",
+            options: [
+              "Application Programming Interface",
+              "Advanced Programming Interface",
+              "Automated Program Integration",
+              "All Purpose Interface"
+            ]
+          },
       // Add more quiz data as needed...
     ];
   
