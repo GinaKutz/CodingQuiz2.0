0d623cff1dd36620cc8a67f8feaba46c1d60a940
GinaKutz <gjkutz@gmail.com>
2023-12-18 15:13:33 -0600

    Quiz reboot

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CodingQuiz2.0
+
+## Description
+
+This is a quiz with questions about coding.
+
+This is a challenging task because I have had numerous attempts and disliked the outcome of each attempt for a different reason.
+
--- assets/script.js
+++ assets/script.js
@@ -1,97 +1,90 @@
 document.addEventListener('DOMContentLoaded', () => {
     const quizData = [
-        {
-            numb: 1,
+            {
+    
             question: "What does HTML stand for?",
             answer: "Hyper Text Markup Language",
-            options: [
-              "Hyper Text Preprocessor",
+            options: ["Hyper Text Preprocessor",
               "Hyper Text Markup Language",
               "Hyper Text Multiple Language",
-              "Hyper Tool Multi Language"
-            ]
+              "Hyper Tool Multi Language"],
+            
           },
             {
-            numb: 2,
+           
             question: "What does CSS stand for?",
             answer: "Cascading Style Sheet",
-            options: [
-              "Common Style Sheet",
+            options: ["Common Style Sheet",
               "Colorful Style Sheet",
               "Computer Style Sheet",
-              "Cascading Style Sheet"
-            ]
+              "Cascading Style Sheet"]
+            
           },
+
             {
-            numb: 3,
+            
             question: "Which of the following is not a programming language?",
             answer: "CSS",
-            options: [
-              "Python",
+            options: ["Python",
               "CSS",
               "Java",
-              "Ruby"
-            ]
+              "Ruby"]
+            
           },
             {
-            numb: 4,
+            
             question: "In Python, what is used to inticate a block of code following a conditional statement or loop",
             answer: "Indentation",
-            options: [
-              "Parenthesis",
+            options: ["Parenthesis",
               "Curly Brackets",
               "Indentation",
-              "Quotation marks"
-            ]
+              "Quotation marks"]
+            
           },
             {
-            numb: 5,
+            
             question: "What does XML stand for?",
             answer: "eXtensible Markup Language",
-            options: [
-              "eXtensible Markup Language",
+            options: ["eXtensible Markup Language",
               "eXecutable Multiple Language",
               "eXTra Multi-Program Language",
-              "eXamine Multiple Language"
-            ]
+              "eXamine Multiple Language"]
+            
           },
         
           {
-            numb: 6,
+            
             question: "Which symbol is used for a single-line comment in most programming language?",
             answer: "//",
-            options: [
-              "//",
+            options: ["//",
               "#",
               "--",
-              "/**/"
-            ]
+              "/**/"]
+            
           },
         
           {
-            numb: 7,
+            
             question: 'What is the purpose of the "git" version control system?',
             answer: "To manage and track changes in files",
-            options: [
-              "To write and execute code",
+            options: ["To write and execute code",
               "To manage and track changes in files",
               "To design user interface",
-              "To optimize website loading speed"
-            ]
+              "To optimize website loading speed"]
+            
           },
         
           {
-            numb: 8,
+           
             question: "What does the acronym API stand for in programming?",
             answer: "Application Programming Interface",
-            options: [
-              "Application Programming Interface",
+            options: ["Application Programming Interface",
               "Advanced Programming Interface",
               "Automated Program Integration",
-              "All Purpose Interface"
-            ]
+              "All Purpose Interface"]
+            
           },
-      // Add more quiz data as needed...
+      
     ];
   
     const startBtn = document.getElementById('startBtn');
